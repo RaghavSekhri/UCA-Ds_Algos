a1c6347edeea4097ee90a431e1cb93e9f45369c9
RaghavSekhri <raghavsekhri2406@gmail.com>
2019-07-27 00:15:26 +0530

    Removing dublicates and returning number of unique elements

---
+++ Remove_Dublicates.cpp
@@ -0,0 +1,24 @@
+#include<iostream>                //remove duplicates
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter size of array : ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter elements of array : ";
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    int j=0;
+    for(int i=1;i<n;i++)
+    {
+        if(arr[i]!=arr[j])
+        {
+            j++;
+            arr[j]=arr[i];
+        }
+    }
+    cout<<"No. of unique elements are : "<<j+1<<"\n";
+    return 0;
+}
+
