2b05d79486edfacdfe06d04856187cc9df1ba7e7
RaghavSekhri <raghavsekhri2406@gmail.com>
2019-10-01 10:44:10 +0530

    JavaScript Basics

--- Javascipt Basics/app.js
+++ Javascipt Basics/app.js
@@ -5,3 +5,23 @@ function hello()
     console.log('hello world');
 }
 hello();
+
+// $$ hoisting of variable $$
+
+var a = 25;
+if(true)
+{
+    var a = 35;
+    console.log(a);
+}
+console.log(a); 
+
+let a = 25
+if(true)
+{
+    let a = 35;
+    console.log(a);
+}
+console.log(a);
+
+// 'let' has local scope and 'var' has global scope
