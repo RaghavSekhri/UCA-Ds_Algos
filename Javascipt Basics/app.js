c92a8ae0cb1fca576a9b1994a5e304a762f89295
RaghavSekhri <raghavsekhri2406@gmail.com>
2019-10-03 09:51:49 +0530

    JavaScript Basics

--- Javascipt Basics/app.js
+++ Javascipt Basics/app.js
@@ -77,3 +77,15 @@ var hello = a => {
     console.log();
 }
 hello(20);
+
+// $$ .map() $$ 
+
+var officers = [
+    { id: 20, name: "Captain Piett" },
+    { id: 24, name: "General Veers" },
+    { id: 56, name: "Admiral Ozzel" },
+    { id: 88, name: "Commander Jerjerrod" }
+  ];
+  
+const officersIds = officers.map(officer => officer.id);
+console.log(officersIds);
