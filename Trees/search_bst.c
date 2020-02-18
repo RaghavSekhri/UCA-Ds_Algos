63404d7b1b5ae62ab7d5bfa32adb65681e553f0e
RaghavSekhri <raghavsekhri2406@gmail.com>
2020-02-18 23:07:12 +0530

    searching a node in a bst

---
+++ Trees/search_bst.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+struct BstNode{
+int data;
+struct BstNode *left;
+struct BstNode *right;
+};
+
+struct BstNode* GetNewNode(int data)
+{
+    struct BstNode* newNode = (struct BstNode*) malloc (sizeof(struct BstNode));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct BstNode* Insert(struct BstNode *root, int data)
+{
+    if(root == NULL)
+        root = GetNewNode(data);
+    else if(data <= root->data)
+        root->left = Insert(root->left, data);
+    else if(data > root->data)
+        root->right = Insert(root->right, data);
+    return root;
+}
+
+int Search(struct BstNode* root, int data)
+{
+    if(root == NULL)
+        return 0;
+    else if(root->data == data)
+        return 1;
+    else if(data <= root->data)
+        return Search(root->left, data);
+    else if(data > root->data)
+        return Search(root->right, data);
+}
+
+int main()
+{
+    struct BstNode *root = NULL;
+    root = Insert(root, 15);
+    root = Insert(root, 10);
+    root = Insert(root, 20);
+    int number; 
+    printf("Enter number to be searched : ");
+    scanf("%d",&number);
+    if(Search(root, number) == 1)
+        printf("Found");
+    else
+        printf("Not Found");
+}
