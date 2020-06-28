43e272338b0a5562b2d3d09061addcc5f29b2383
Raghav Sekhri <raghavsekhri2406@gmail.com>
2020-06-28 19:33:06 +0530

    Update Nth Node From End of Linked List.c

--- Geeks For Geeks/Nth Node From End of Linked List.c
+++ Geeks For Geeks/Nth Node From End of Linked List.c
@@ -1,20 +1,36 @@
 int getNthFromLast(Node *head, int n)
 {
-    Node *temp = head;
-    int count=0;
-    while(temp != NULL)
+//     Node *temp = head;
+//     int count=0;
+//     while(temp != NULL)
+//     {
+//         count++;
+//         temp = temp->next;
+//     }
+//     int value = count-n;
+//     int count1=0;
+//     while(head != NULL)
+//     {
+//         if(count1 == value)
+//             return head->data;
+//         count1++;
+//         head = head->next;
+//     }
+//     return -1;
+    if(head == NULL)
+        return;
+    Node *first = head;
+    for(int i=0;i<n;i++)
     {
-        count++;
-        temp = temp->next;
+        if(first == NULL)
+            return;
+        first = first->next;
     }
-    int value = count-n;
-    int count1=0;
-    while(head != NULL)
+    Node *second = head;
+    while(first != NULL)
     {
-        if(count1 == value)
-            return head->data;
-        count1++;
-        head = head->next;
+        second = second->next;
+        first = first->next;
     }
-    return -1;
+    cout<<second->data;
 }
